b99e820a0bb97f599e01ae8f987d85c418ed01e4
Bizhunov <bizhunovsergei@gmail.com>
2021-07-22 21:54:27 +0300

    HW for 5 class

---
+++ FirstConsoleProject/FirstConsoleProject/Lab2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int z;
+            Mathan mathan = new Mathan();
+            mathan.Add(out x, out z);
+            mathan.Calculation(x, z);
+            mathan.Show(x,z);
+        }
+    }
+    class Mathan
+    {
+        public void Add(out int x,out int z)
+        {
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение z:");
+            z = Convert.ToInt32(Console.ReadLine());
+        }
+        public double Calculation(int x,int z)
+        {
+            double f;
+            if ((-5 < z) && (z < 0))
+            {
+                f = (Math.Sqrt(x) + 2);
+                return f;
+            }
+            else if (z <= -5)
+            {
+                f = 2.5 * z;
+                return f;
+            }
+            else
+            {
+                f = ((Math.Pow(x, 3) + 2)) / z;
+                return f;
+            }
+        }
+        public void Show(int x,int z)
+        {
+            double f = Calculation(x, z);
+            Console.WriteLine(f);
+        }
+    }
+}
---
+++ FirstConsoleProject/FirstConsoleProject/lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z;
+            int y;
+            int x;
+            Mathan mathan = new Mathan();
+            mathan.Add(out z, out y, out x);
+            mathan.Calculation(z, y, x);
+            mathan.Show(z, y, x);
+        }
+    }
+    class Mathan
+    {
+        public void Add(out int z,out int y,out int x)
+        {
+            Console.WriteLine("Введите значение z:");
+            z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToInt32(Console.ReadLine());
+        }
+        public double Calculation(int z,int y,int x)
+        {
+            double b;
+            b = z * (y - (Math.Pow(x + y, -2)));
+            return b;
+        }
+        public void Show(int z,int y,int x)
+        {
+            double b = Calculation(z, y, x);
+            Console.WriteLine(b);
+        }
+    }
+}
