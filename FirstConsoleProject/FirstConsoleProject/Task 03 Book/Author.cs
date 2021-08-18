e81699e5b58af84f0223b115842c26e0099a107e
Bizhunov <bizhunovsergei@gmail.com>
2021-08-18 20:48:50 +0200

    Task 03 ITVDN

---
+++ FirstConsoleProject/FirstConsoleProject/Task 03 Book/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03_Book
+{
+    class Author
+    {
+        private string _text;
+
+        public Author(string text)
+        {
+            _text = text;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(_text);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ FirstConsoleProject/FirstConsoleProject/Task 03 Book/Title.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03_Book
+{
+    class Title
+    {
+
+        private string _text;
+
+        public Title(string text)
+        {
+            _text = text;
+        }
+
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(_text);
+            Console.ResetColor();
+        }
+    }
+}
