5575c15c0d6a68a866a358bf52c218122c3d2b31
Bizhunov <bizhunovsergei@gmail.com>
2021-08-20 19:35:10 +0300

    Task 04 ITVDN

---
+++ FirstConsoleProject/FirstConsoleProject/Task 04/Figure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04
+{
+    class Figure
+    {
+        private double _side1;
+        private double _side2;
+        private int _side3;
+
+        public Figure(double side1, double side2)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+    }
+}
---
+++ FirstConsoleProject/FirstConsoleProject/Task 04/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04
+{
+    class Point
+    {
+        private double _point1;
+        private double _point2;
+        private int _point3;
+        public double Point1; 
+        public Point(double point1, double point2)
+        {
+            _point1 = point1;
+            _point2 = point2;
+        }
+    }
+}
