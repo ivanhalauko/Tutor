15a423c0a9f4a9c8217cff5c5c409a84ad316465
Sergey <bizhunovsergei@gmail.com>
2022-06-05 22:29:33 +0300

    Position controller added and Position view

--- Diplom/src/AccountingWorksIinstruments.Web/Controllers/HomeController.cs
+++ Diplom/src/AccountingWorksIinstruments.Web/Controllers/HomeController.cs
@@ -36,27 +36,7 @@ namespace AccountingWorksIinstruments.Web.Controllers
             return View();
         }
 
-        public IActionResult Positions()
-        {
-            var entities = _positionService.ReadAll();
-            var positions = _mapperConfig.Mapper.Map<IEnumerable<Position>, IEnumerable<PositionViewModel>>(entities);
-            return View(positions);
-        }
-
-        public IActionResult GetByIdPosition(int Id)
-        {
-            var entities = _positionService.GetById(Id);
-            var position = _mapperConfig.Mapper.Map<IEnumerable<Position>, IEnumerable<PositionViewModel>>(entities);
-            return View(position);
-        }
-
-        public IActionResult UpdatePosition(int id)
-        {
-            
-            var entities = _positionService.GetById(id);
-            var positions = _mapperConfig.Mapper.Map<IEnumerable<Position>, IEnumerable<PositionViewModel>>(entities).FirstOrDefault();
-            return View(positions);
-        }
+        
 
         //public IActionResult AddPosition(Position position)
         //{
@@ -65,71 +45,7 @@ namespace AccountingWorksIinstruments.Web.Controllers
         //    return;
         //}
 
-        [HttpPost]
-        public IActionResult UpdatePosition( IFormCollection collection)
-        {
-            try
-            {
-                int id = Convert.ToInt32(collection["Id"]);
-                string position = Convert.ToString(collection["Name"]);
-                Position positionEntity = new Position(id, position);
-                _positionService.Update(positionEntity);
-                return RedirectToAction("Positions");
-            }
-            catch (Exception ex)
-            {
-
-                throw new Exception(ex.Message);
-            }
-        }
-        public IActionResult DeletePosition(int id)
-        {
-            var entities = _positionService.GetById(id);
-            var positions = _mapperConfig.Mapper.Map<IEnumerable<Position>, IEnumerable<PositionViewModel>>(entities).FirstOrDefault();
-            return View(positions);
-        }
-
-        [HttpPost]
-        public IActionResult DeletePosition(IFormCollection collection)
-        {
-            try
-            {
-                int id = Convert.ToInt32(collection["Id"]);
-                string position = Convert.ToString(collection["Name"]);
-                Position positionEntity = new Position(id, position);
-                _positionService.DeleteAll(positionEntity);
-                return RedirectToAction("Positions");
-            }
-            catch (Exception ex)
-            {
-
-                throw new Exception(ex.Message);
-            }
-        }
-        public IActionResult CreatePosition()
-        {
-            var entities = new Position(0,null);
-            var positions = _mapperConfig.Mapper.Map<Position, PositionViewModel>(entities);
-            return View(positions);
-        }
-
-        [HttpPost]
-        public IActionResult CreatePosition(IFormCollection collection)
-        {
-            try
-            {
-                int id = Convert.ToInt32(collection["Id"]);
-                string position = Convert.ToString(collection["Name"]);
-                Position positionEntity = new Position(id, position);
-                _positionService.Add(positionEntity);
-                return RedirectToAction("Positions");
-            }
-            catch (Exception ex)
-            {
-
-                throw new Exception(ex.Message);
-            }
-        }
+       
         
 
        
--- Diplom/src/AccountingWorksIinstruments.Web/Controllers/PositionController.cs
+++ Diplom/src/AccountingWorksIinstruments.Web/Controllers/PositionController.cs
@@ -16,6 +16,100 @@ namespace AccountingWorksIinstruments.Web.Controllers
     {
         private readonly IMapperConfig _mapperConfig;
         private readonly IPositionService _positionService;
+
+        public PositionController(IMapperConfig mapConfig, IPositionService positionService)
+        {
+            _mapperConfig = mapConfig;
+            _positionService = positionService;
+        }
+
+        public IActionResult Positions()
+        {
+            var entities = _positionService.ReadAll();
+            var positions = _mapperConfig.Mapper.Map<IEnumerable<Position>, IEnumerable<PositionViewModel>>(entities);
+            return View(positions);
+        }
+
+        public IActionResult GetByIdPosition(int Id)
+        {
+            var entities = _positionService.GetById(Id);
+            var position = _mapperConfig.Mapper.Map<IEnumerable<Position>, IEnumerable<PositionViewModel>>(entities);
+            return View(position);
+        }
+
+        public IActionResult UpdatePosition(int id)
+        {
+
+            var entities = _positionService.GetById(id);
+            var positions = _mapperConfig.Mapper.Map<IEnumerable<Position>, IEnumerable<PositionViewModel>>(entities).FirstOrDefault();
+            return View(positions);
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePosition(IFormCollection collection)
+        {
+            try
+            {
+                int id = Convert.ToInt32(collection["Id"]);
+                string position = Convert.ToString(collection["Name"]);
+                Position positionEntity = new Position(id, position);
+                _positionService.Update(positionEntity);
+                return RedirectToAction("Positions");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public IActionResult DeletePosition(int id)
+        {
+            var entities = _positionService.GetById(id);
+            var positions = _mapperConfig.Mapper.Map<IEnumerable<Position>, IEnumerable<PositionViewModel>>(entities).FirstOrDefault();
+            return View(positions);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePosition(IFormCollection collection)
+        {
+            try
+            {
+                int id = Convert.ToInt32(collection["Id"]);
+                string position = Convert.ToString(collection["Name"]);
+                Position positionEntity = new Position(id, position);
+                _positionService.DeleteAll(positionEntity);
+                return RedirectToAction("Positions");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public IActionResult CreatePosition()
+        {
+            var entities = new Position(0, null);
+            var positions = _mapperConfig.Mapper.Map<Position, PositionViewModel>(entities);
+            return View(positions);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePosition(IFormCollection collection)
+        {
+            try
+            {
+                int id = Convert.ToInt32(collection["Id"]);
+                string position = Convert.ToString(collection["Name"]);
+                Position positionEntity = new Position(id, position);
+                _positionService.Add(positionEntity);
+                return RedirectToAction("Positions");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
         public IActionResult Index()
         {
             return View();
